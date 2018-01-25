3943a57e4acda238e0c6c1a6c95701a5c9c270ff
Richard Wohlbold <richard.wohlbold@gmail.com>
2018-01-25 10:02:14 -0600

    Fixes 2 and 3

--- 02.py
+++ 02.py
@@ -1,18 +1,7 @@
-def phone(numbers):
-    dallas, tallahassee, birmingham, nashville, other = 0,0,0,0,0
-    for number in numbers:
-        area = number // 10000000
-        if area == 469:
-            dallas += 1
-        elif area == 850:
-            tallahassee += 1
-        elif area == 205:
-            birmingham += 1
-        elif area == 615:
-            nashville += 1
-        else:
-            other += 1
-    return [dallas, tallahassee, birmingham, nashville, other]
+#!/usr/bin/python3
+
+def contains(list, number):
+    return number in list
 
 
-print(phone([2057281628, 4692581647, 1234567890]))
+print(contains([1,2,3], 2))
---
+++ 03.py
@@ -0,0 +1,18 @@
+def phone(numbers):
+    dallas, tallahassee, birmingham, nashville, other = 0,0,0,0,0
+    for number in numbers:
+        area = number // 10000000
+        if area == 469:
+            dallas += 1
+        elif area == 850:
+            tallahassee += 1
+        elif area == 205:
+            birmingham += 1
+        elif area == 615:
+            nashville += 1
+        else:
+            other += 1
+    return [dallas, tallahassee, birmingham, nashville, other]
+
+
+print(phone([2057281628, 4692581647, 1234567890]))
