990ce97d7af15a51daafc8be9a3a45bde1bc3faa
Richard Wohlbold <richard.wohlbold@gmail.com>
2018-01-26 11:06:29 -0600

    Finished all vignettes

---
+++ 22.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import math
+
+def expand(frac, n):
+    lcm = frac[1] * n // math.gcd(frac[1], n)
+    frac[0] *= lcm
+    frac[1] *= lcm
+
+
+def reduce(frac):
+    gcd = math.gcd(frac[0], frac[1])
+    frac[0] //= gcd
+    frac[1] //= gcd
+    
+
+def decompose(frac):
+    f = list(frac)
+    n = f[0] / f[1]
+    units = []
+    while f[0] > 0:
+        units.append((1, math.ceil(f[1] / f[0])))
+        f[0], f[1] = -f[1] % f[0], f[1] * math.ceil(f[1] / f[0])
+        reduce(f)
+    return units
+
+print(decompose((6,7)))
+
