a68df776b0505ceb7342588f755df115f87f7588
Richard Wohlbold <richard.wohlbold@gmail.com>
2018-01-29 14:19:18 -0600

    Added comments to 05

--- 05.py
+++ 05.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
+# Input: Coordinates of diagonal endpoints: [x1, y1, x2, y2]
 def area(diags):
+    # Width of rectangle
     x = abs(diags[0] - diags[2])
+    # Height of rectangle
     y = abs(diags[1] - diags[3])
-    return x*y
+    return x * y
 
 
 print(area([2, 5, 5, 2]))
