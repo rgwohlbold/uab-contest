7410ba9b3064bd0050dd8a7dc7a62ba5eaac2c58
FunkyQChicken <spencergrobinson@gmail.com>
2018-01-29 17:58:45 -0600

    made 07 *slightly* more readable

--- 07.py
+++ 07.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def check(number):
-    digits = list(map(lambda x: int(x), str(number)))
-    odds = [ digits[x] for x in range(0, 15, 2) ]
-    evens = [ digits[x] for x in range(1, 15, 2) ]
-    m4 = len(list(filter(lambda x: x > 4, odds)))
+    digits = list(map(int, str(number)))
+    odds = digits[0::2]
+    evens = digits[1::2]
+    m4 = len([x for x in odds if x>4])
     total = 2 * sum(odds) + sum(evens) + m4
     return 10 - (total % 10)
 
