e4ffa4f70c3e5cdca8b5e4a8fcb21038990765cc
FunkyQChicken <spencergrobinson@gmail.com>
2018-01-30 14:07:31 -0600

    changed 30 to run in O(n) instead of O(n^2)

--- 30.py
+++ 30.py
@@ -1,37 +1,41 @@
 #!/usr/bin/python3
 
-def flow(source, target, cups):
-    # If there is no valley, no additional cups are filled
-    if cups[source] >= cups[target]:
-        return 0
-    # Get the direction by the sign of the difference 
-    direction = (target - source) // abs(target - source)
+def cups(heights):
+    # the running count of filled 'water tiles'
+    filled = 0
+    # the highest cup tower hit so far 
+    maxheight = 0
+    # the running count of 'water tiles' that might be filled
+    potentialfill = 0
 
-    level = cups[target]
-    current = target - direction
-    total = 0
-    while cups[current] < level:
-        # Add the difference between the current and the target cup to the total
-        total += (level - cups[current])
-        # Set the current cup to the target water level
-        cups[current] = level
-        current -= direction
-    return total
+    for x in heights:
+        # if the cup tower im at currently is larger than my previous maxheight
+        # then i should add all of my potential fill to my filled becuase i 
+        # have confirmed the valley is blocked off and holds water
+        if x >= maxheight:
+            maxheight = x
+            filled += potentialfill
+            potentialfill = 0
+        # else i should add the distance from the max height to the valley floor
+        # to simulate what the water level would be if it is indeed filled with water
+        else:
+            potentialfill += (maxheight - x)
 
+    # i then repeat all of this but approaching from the opposite side so that
+    # i get the valleys to the left of the highest cup tower.
+    maxheight = 0
+    potentialfill = 0
+    for x in heights[::-1]:
+        if x >= maxheight:
+            maxheight = x
+            filled += potentialfill
+            potentialfill = 0
+        else:
+            potentialfill += (maxheight - x)
+    # all the top cups will be filled regardless of the cups height or arrangement
+    # so the length is added to represent one unit of water filled for each cup.
+    filled += len(heights)
 
-def cups(list):
-    first = list.index(max(list))
-    total = len(list)
-    
-    i = first + 1
-    while i < len(list) - 1:
-        total += flow(i, i+1, list)
-        i += 1
-
-    i = first - 1
-    while i > 0:
-        total += flow(i, i-1, list)
-        i -= 1
-    return total
+    return filled
 
 print(cups([3,1,2,1,4]))
