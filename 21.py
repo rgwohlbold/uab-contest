61b9c033b74f3c5f26500cf15aaab1aa32ae6d7e
Richard Wohlbold <richard.wohlbold@gmail.com>
2018-01-31 09:34:08 -0600

    Fixed 21 to run correctly in O(1)

--- 21.py
+++ 21.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def pushup(score):
-    assert(score % 11 is 0)
-    return score * 2 - 11
+    points = score // 11
+    return points * (points + 1) // 2 * 11
 
 print(pushup(33))
